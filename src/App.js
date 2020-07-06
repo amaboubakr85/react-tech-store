9e266135ce1b042ed6840519c68a4f030d6f2e17
amaboubakr85 <a.m.aboubakr85@gmail.com>
2020-07-07 00:38:50 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, {Component} from 'react';
-import "./App.css";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "./App.css";
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import Default from "./pages/Default";
+import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage";
+import { Route, Switch } from "react-router-dom";
+
 class App extends Component {
-    render() {
-        return (
-            <div>
- <h1>hello from app </h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <>
+        {/* navbar , sidebar , cart, footer */}
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
+          <Route path="/products" exact component={Products}></Route>
+          <Route path="/products/:id" component={SingleProduct}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
